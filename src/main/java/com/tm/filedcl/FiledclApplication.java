6076d9deedff8cbae3e61ded87ccba332d712372
tuming <812611265@qq.com>
2019-06-28 14:40:28 +0800

    Initial commit

---
+++ src/main/java/com/tm/filedcl/FiledclApplication.java
@@ -0,0 +1,13 @@
+package com.tm.filedcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiledclApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiledclApplication.class, args);
+    }
+
+}
